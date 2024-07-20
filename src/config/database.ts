7bddf484b9fa64edda91e713bbf2492ce993ee1a
Prashanth Sekar <prashanth.ssa@gmail.com>
2024-07-20 22:23:25 +0530

    -feat: updated the typeORM

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,29 @@
-import { Sequelize } from 'sequelize';
-import { config as dotenvConfig } from 'dotenv';
+import { createConnection } from "typeorm";
+import dotenv  from 'dotenv';
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { parse } from "path";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
-dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.prod' });
+} else {
+  dotenv.config({ path: '.env.dev' });
+}
 
-const databaseUrl = process.env.DATABASE_URL as string;
-const jwtSecret = process.env.JWT_SECRET as string;
-
-export const sequelize = new Sequelize(databaseUrl, {
-  dialect: 'postgres',
-  logging: process.env.NODE_ENV === 'development' ? console.log : false,
-});
+(async function () {
+  let options: PostgresConnectionOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [parse(require.resolve("@aimingle/entity")).dir + "/entity/*.js"],
+    synchronize: true,
+    logging: process.env.NODE_ENV === 'development' ? true : false,
+    namingStrategy: new SnakeNamingStrategy(),
+  }
+  await createConnection(options).then(() => {
+    console.log("Database connection established successfully :"+ process.env.PORT);
+  }).catch(error => console.log(error));
+})();
